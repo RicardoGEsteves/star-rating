6f3cca0bf4e40418ab18055192529b897dcfeaa4
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-11-17 20:15:00 +0000

    push code base

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+
+    <Test />
+  </React.StrictMode>
+);
